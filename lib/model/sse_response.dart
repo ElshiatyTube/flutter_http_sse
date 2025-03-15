03d0ebc83cd37443a8162ef9d9472f97afe1d765
Youssef Elshiaty N. <155442184+youssefelshiaty@users.noreply.github.com>
2025-03-15 18:04:50 +0200

    Refactor

--- lib/model/sse_response.dart
+++ lib/model/sse_response.dart
@@ -12,44 +12,40 @@ class SSEResponse<T> {
 
   /// Parses an SSE event string into an SSEResponse object
   factory SSEResponse.parse(String rawResponse, {Function(dynamic)? fromJson}) {
-    try {
-      String? id;
-      String? event;
-      String? comment;
-      StringBuffer jsonDataBuffer = StringBuffer();
+    String? id;
+    String? event;
+    String? comment;
+    StringBuffer jsonDataBuffer = StringBuffer();
 
-      // Splitting the SSE response into lines
-      for (final line in rawResponse.split('\n')) {
-        if (line.startsWith('id: ')) {
-          id = line.substring(4).trim();
-        } else if (line.startsWith('event: ')) {
-          event = line.substring(7).trim();
-        } else if (line.startsWith('data: ')) {
-          if (jsonDataBuffer.isNotEmpty) jsonDataBuffer.write('\n');
-          jsonDataBuffer.write(line.substring(6).trim());
-        } else if (line.startsWith(':')) {
-          comment = line.substring(1).trim();
-        }
+    // Splitting the SSE response into lines
+    for (final line in rawResponse.split('\n')) {
+      if (line.startsWith('id: ')) {
+        id = line.substring(4).trim();
+      } else if (line.startsWith('event: ')) {
+        event = line.substring(7).trim();
+      } else if (line.startsWith('data: ')) {
+        if (jsonDataBuffer.isNotEmpty) jsonDataBuffer.write('\n');
+        jsonDataBuffer.write(line.substring(6).trim());
+      } else if (line.startsWith(':')) {
+        comment = line.substring(1).trim();
       }
-
-      // Convert buffered data into JSON object if available
-      T? parsedData;
-      final jsonData = jsonDataBuffer.toString();
-      if (jsonData.isNotEmpty) {
-        try {
-          final decodedData = json.decode(jsonData);
-          parsedData =
-              fromJson != null ? fromJson(decodedData) : decodedData as T;
-        } catch (e) {
-          return SSEResponse.successWithRawResponse(
-              rawData: json.decode(jsonData), rawResponse: rawResponse);
-        }
-      }
-      return SSEResponse(
-          id ?? '', event ?? '', comment ?? '', parsedData, rawResponse);
-    } catch (e) {
-      return SSEResponse.successWithRawResponse(rawResponse: rawResponse);
     }
+
+    // Convert buffered data into JSON object if available
+    T? parsedData;
+    final jsonData = jsonDataBuffer.toString();
+    if (jsonData.isNotEmpty) {
+      try {
+        final decodedData = json.decode(jsonData);
+        parsedData =
+        fromJson != null ? fromJson(decodedData) : decodedData as T;
+      } catch (e) {
+        return SSEResponse.successWithRawResponse(
+            rawData: json.decode(jsonData), rawResponse: rawResponse);
+      }
+    }
+    return SSEResponse(
+        id ?? '', event ?? '', comment ?? '', parsedData, rawResponse);
   }
 
   factory SSEResponse.empty() {
