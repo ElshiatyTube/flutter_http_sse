5e6f39b86696117f8188d155ff6bcea521887aa4
Youssef Elshiaty N. <155442184+youssefelshiaty@users.noreply.github.com>
2025-03-15 18:27:40 +0200

    Refactor

--- lib/client/sse_client_impl.dart
+++ lib/client/sse_client_impl.dart
@@ -1,132 +1,66 @@
-import 'dart:async';
 import 'dart:convert';
-import 'package:http/http.dart' as http;
-import '../model/sse_request.dart';
-import '../model/sse_response.dart';
-import 'i_sse_client.dart';
 
-class SSEClient<T> extends ISSEClient {
-  final Map<String, _SSEConnection<T>> _connections = {};
+class SSEResponse<T> {
+  final String id;
+  final String event;
+  final String comment;
+  final T? data;
+  final String rawResponse;
+
+  const SSEResponse(
+      this.id, this.event, this.comment, this.data, this.rawResponse);
+
+  /// Parses an SSE event string into an SSEResponse object
+  factory SSEResponse.parse(String rawResponse, {Function(dynamic)? fromJson}) {
+    String? id;
+    String? event;
+    String? comment;
+    StringBuffer jsonDataBuffer = StringBuffer();
+
+    // Splitting the SSE response into lines
+    for (final line in rawResponse.split('\n')) {
+      if (line.startsWith('id: ')) {
+        id = line.substring(4).trim();
+      } else if (line.startsWith('event: ')) {
+        event = line.substring(7).trim();
+      } else if (line.startsWith('data: ')) {
+        if (jsonDataBuffer.isNotEmpty) jsonDataBuffer.write('\n');
+        jsonDataBuffer.write(line.substring(6).trim());
+      } else if (line.startsWith(':')) {
+        comment = line.substring(1).trim();
+      }
+    }
+
+    // Ensure non-nullable fields have default values
+    id ??= '';
+    event ??= '';
+    comment ??= '';
+
+    // Convert buffered data into JSON object if available
+    T? parsedData;
+    final jsonData = jsonDataBuffer.toString();
+    if (jsonData.isNotEmpty) {
+      try {
+        final decodedData = json.decode(jsonData);
+        parsedData = fromJson != null ? fromJson(decodedData) : decodedData as T;
+      } catch (e) {
+        return SSEResponse.raw(rawResponse);
+      }
+    }
+
+    return SSEResponse(id, event, comment, parsedData, rawResponse);
+  }
+
+  factory SSEResponse.empty() {
+    return SSEResponse('', '', '', null, '');
+  }
+
+  factory SSEResponse.raw(String rawResponse) {
+    return SSEResponse('', '', '', rawResponse as T, rawResponse);
+  }
 
   @override
-  void close({String? connectionId}) {
-    if (connectionId != null) {
-      _connections[connectionId]?.close();
-      _connections.remove(connectionId);
-    } else {
-      for (var connection in _connections.values) {
-        connection.close();
-      }
-      _connections.clear();
-    }
-  }
-
-  @override
-  Stream<SSEResponse<T>> connect(String connectionId, SSERequest request, {Function(dynamic)? fromJson}) {
-    return subscribeToSSE(connectionId, request, fromJson: fromJson);
-  }
-
-  Stream<SSEResponse<T>> subscribeToSSE(String connectionId, SSERequest request, {Function(dynamic)? fromJson}) {
-    if (_connections.containsKey(connectionId)) {
-      return _connections[connectionId]!.stream;
-    }
-
-    final connection = _SSEConnection<T>(request, fromJson);
-    _connections[connectionId] = connection;
-    return connection.stream;
-  }
-}
-
-class _SSEConnection<T> {
-  final SSERequest request;
-  final Function(dynamic p1)? fromJson;
-  final StreamController<SSEResponse<T>> _controller = StreamController.broadcast();
-  http.Client? _client;
-  int _retryCount = 0;
-  static const int _maxRetries = 5;
-  static const Duration _initialDelay = Duration(seconds: 2);
-
-  _SSEConnection(this.request, this.fromJson) {
-    _connect();
-  }
-
-  void _connect() {
-    _client = http.Client();
-    var httpRequest = http.Request(request.requestType.value, request.getRequestUri);
-
-    if (request.headers != null) {
-      httpRequest.headers.addAll(request.headers!);
-    }
-    if (request.body != null) {
-      httpRequest.body = json.encode(request.body);
-    }
-
-    _client!.send(httpRequest).then((response) {
-      if (response.statusCode >= 500) {
-        _handleError("Failed to connect to server. Status code: ${response.statusCode}");
-        return;
-      }
-
-      _retryCount = 0;
-
-      response.stream.transform(utf8.decoder).transform(const LineSplitter()).listen(
-            (String line) {
-          if (_controller.isClosed) return;
-          if (line.isEmpty) {
-            _controller.add(SSEResponse<T>.empty());
-          } else {
-            var sseRes =  SSEResponse<T>.parse(line, fromJson: fromJson);
-            _controller.add(sseRes);
-            request.onData(sseRes);
-          }
-        },
-        onDone: () {
-          request.onDone?.call();
-        },
-        onError: (error) {
-          _handleError(error);
-        },
-        cancelOnError: true,
-      );
-    }).catchError((error) {
-      _handleError(error);
-    });
-  }
-
-  void _handleError(dynamic error) {
-    print("Error connecting to server via SSE: $error");
-    if (_controller.isClosed) return;
-    _controller.addError(error);
-    request.onError?.call(error.toString());
-
-    if (request.retry) {
-      _handleRetry();
-    } else {
-      close();
-    }
-  }
-
-  void _handleRetry() {
-    if (_retryCount >= _maxRetries) {
-      close();
-      return;
-    }
-
-    _retryCount++;
-    Duration retryDelay = _initialDelay * _retryCount; // Exponential backoff
-
-    Future.delayed(retryDelay, () {
-      if (!_controller.isClosed) {
-        _connect();
-      }
-    });
-  }
-
-  Stream<SSEResponse<T>> get stream => _controller.stream;
-
-  void close() {
-    if (_controller.isClosed) return;
-    _controller.close();
-    _client?.close();
+  String toString() {
+    return 'SSEResponse{id: $id, event: $event, comment: $comment, data: ${data.toString()}, rawResponse: $rawResponse}';
   }
 }
